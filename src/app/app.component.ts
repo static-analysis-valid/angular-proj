d36098c98d6abe0df4f1f7cb6bc80eb7e58d5926
static-analysis-valid <akosanovic@net.hr>
2021-02-23 10:57:14 +0100

    Expected warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+  
+// Expected warning: Import declared but unused 
+// Expected warning: File is not a module 
+import { ABSOLUTE_DOMAIN_URL, DEFAULT_COOKIE_SETTINGS } from "./globals";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StaticAnalysis';
+  wrongValueAssigned: number;
+
+  // Expected warning: property is declared but never used
+  constructor(abc) {
+
+  	let nullValue = null;
+    // Expected warning: Can't read property of null or undefined 
+    // Expected warning: Unused local variable 
+    var firstValueOfNull = nullValue.first; 
+
+    // Expected warning: Cannot assign value to the const  
+  	const constVariableReasigned = 123;
+    constVariableReasigned = "new value";
+
+    // Expected warning: console.log 
+    // Expected warning: Unterminated statement 
+    console.log('warn about console log ')
+
+    // Expected warning: Type number is not assignable to type string 
+    this.wrongValueAssigned = Math.floor(Math.random() * 100); 
+  }
+
+  
+  // Expected warning: Unused Method 
+  emptyMethod() {
+
+  }
+
+
 }
